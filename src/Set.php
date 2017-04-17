86ccf856aea34c90f8198b6fdc6d7f3913950835
Jason McCreary <jason@pureconcepts.net>
2017-04-17 15:39:11 -0400

    Test drive size()

--- src/Set.php
+++ src/Set.php
@@ -4,6 +4,7 @@ namespace Tdd;
 
 class Set {
   private $isEmpty = true;
+  private $size = 0;
 
   public function isEmpty() {
     return $this->isEmpty;
@@ -11,5 +12,10 @@ class Set {
 
   public function add() {
     $this->isEmpty = false;
+    ++$this->size;
+  }
+
+  public function size() {
+    return $this->size;
   }
 }
--- tests/SetTest.php
+++ tests/SetTest.php
@@ -14,4 +14,20 @@ class SetTest extends TestCase
         $oneSet->add('something');
         $this->assertFalse($oneSet->isEmpty());
     }
+
+    public function testSize()
+    {
+      $emptySet = new Set();
+      $this->assertSame(0, $emptySet->size());
+
+      $oneSet = new Set();
+      $oneSet->add('something');
+      $this->assertSame(1, $oneSet->size());
+
+      $manySet = new Set();
+      $manySet->add('one');
+      $manySet->add(2);
+      $manySet->add('3');
+      $this->assertSame(3, $manySet->size());
+    }
 }
