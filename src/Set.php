2794bb4aa41c10c03dc9b499a035fd65c694abce
Jason McCreary <jason@pureconcepts.net>
2017-04-20 13:25:14 -0500

    Beautify to PSR-2 code style

--- src/Set.php
+++ src/Set.php
@@ -2,36 +2,43 @@
 
 namespace Tdd;
 
-class Set {
-  private $items = [];
+class Set
+{
+    private $items = [];
 
-  public function isEmpty() {
-    return $this->size() === 0;
-  }
-
-  public function add($item) {
-      if (!$this->contains($item)) {
-          $this->items[] = $item;
-      }
-  }
-
-  public function size() {
-    return count($this->items);
-  }
-
-  public function contains($item) {
-    return $this->indexOf($item) !== false;
-  }
-
-  public function remove($item) {
-    $index = $this->indexOf($item);
-
-    if ($index !== false) {
-        unset($this->items[$index]);
+    public function isEmpty()
+    {
+        return $this->size() === 0;
     }
-  }
 
-  private function indexOf($item) {
-    return array_search($item, $this->items, true);
-  }
+    public function add($item)
+    {
+        if (!$this->contains($item)) {
+            $this->items[] = $item;
+        }
+    }
+
+    public function size()
+    {
+        return count($this->items);
+    }
+
+    public function contains($item)
+    {
+        return $this->indexOf($item) !== false;
+    }
+
+    public function remove($item)
+    {
+        $index = $this->indexOf($item);
+
+        if ($index !== false) {
+            unset($this->items[$index]);
+        }
+    }
+
+    private function indexOf($item)
+    {
+        return array_search($item, $this->items, true);
+    }
 }
